4d6cc62c9934e86966bbf82bc57792e4bb520ff5
= <chips_code@outlook.com>
2024-07-10 01:06:25 +0800

    update

--- NoodlesDemoGame/Code/main_loop.cpp
+++ NoodlesDemoGame/Code/main_loop.cpp
@@ -14,35 +14,87 @@ struct DefaultNoodleContext : public Context
 	void SubContextRef() const override {}
 };
 
+struct TopFormEventResponder : public Dumpling::FormEventResponder
+{
+	void AddFormEventResponderRef() const override {}
+	void SubFormEventResponderRef() const override {}
+	FormEventRespond Respond(Form& interface, FormEvent event) override
+	{
+		if(event.message == FormEventEnum::DESTROY)
+		{
+			Form::PostFormQuitEvent();
+		}
+		return FormEventRespond::Default;
+	}
+};
+
+
 int main()
 {
 
-	auto form = Form::Create();
+	DefaultNoodleContext n_context;
+
+	TopFormEventResponder respond;
+	auto form = Form::Create(&respond);
 
 	FormProperty pro;
 	pro.title = u8"NoodlesDemoGame";
 
-	form->Init(pro);
 
-	while(true)
-	{
-		bool need_quit = false;
-		while(
-			Form::PeekMessageEventOnce([&](Form*, FormEvent event)->FormEventRespond
+	Task::TaskContext context;
+
+	context.AddGroupThread({1}, 1);
+	auto requireID = context.GetRandomThreadIDFromGroup(1);
+	std::promise<void> pro1;
+
+	auto fur1 = pro1.get_future();
+
+	Task::TaskFilter filter{Task::Priority::High, Task::Category::THREAD_TASK, 0, *requireID};
+
+	n_context.CreateAndAddAtomaticSystem(
+		[](Noodles::ExecuteContext& ex_context)
 		{
-			if(event.message == FormEventEnum::DESTORYED)
-			{
-				need_quit = true;
-			}
-				return FormEventRespond::Default;
-		})
-			)
-		{
-			
+			while(
+				Form::PeekMessageEventOnce([&](Form*, FormEvent event, FormEventRespond)
+					{
+						if(event.message == FormEventEnum::QUIT)
+						{
+							ex_context.noodles_context.Quit();
+						}
+						return FormEventRespond::Default;
+					}
+				)
+				)
+				{
+					
+				}
+		},
+		SystemNodeProperty{
+			Priority{},
+			Property{u8"form_message", u8"gro"},
+			nullptr,
+			filter
 		}
-		if(need_quit)
-			break;
-	}
+	);
+
+	auto init_task = Task::Task::CreateLambdaTask([&](Task::ExecuteStatus& status, Task::Task::Ptr)
+	{
+		form->Init(pro);
+		pro1.set_value();
+	});
+
+	context.CommitTask(init_task, Task::TaskProperty{
+		u8"init form",
+		{},
+		filter
+	});
+
+	fur1.get();
+
+	n_context.Commited(context, {});
+
+	context.ProcessTaskUntillNoExitsTask({});
+	
 	
 
 	return 0;
